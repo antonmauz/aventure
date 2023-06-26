ac07e113f7870498dd4d95c820d1ba0cd06cf816
Christoph Dansard <christoph.dansard@tum.de>
2023-06-26 16:40:01 +0200

    Address with user

--- src/controllers/authentication/signUp.ts
+++ src/controllers/authentication/signUp.ts
@@ -1,18 +1,30 @@
 import express from "express";
-import { DatabaseUser } from "../../services/database/model/DatabaseUser";
 import { hashSync } from "bcryptjs";
+import { databaseService } from "@services";
 
 export const signUp = async (req: express.Request, res: express.Response) => {
-  await DatabaseUser.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: hashSync(req.body.password, 8),
-  })
+  console.log(req.body);
+  
+  await databaseService
+    .createUser({
+      firstName: req.body.firstName,
+      surname: req.body.surname,
+      email: req.body.email,
+      password: hashSync(req.body.password, 8),
+      disabilityVerification: req.body.disabilityVerification
+        ? {
+            idImage: req.body.disabilityVerification.idImage,
+            userImage: req.body.disabilityVerification.userImage,
+            status: "new",
+          }
+        : undefined,
+    })
     .then(() => {
       // TODO return user and token
       res.send({ message: "User was registered successfully!" });
     })
     .catch((error) => {
+      console.log(error);
       if (error) {
         res.status(500).send({ message: error });
         return;
--- src/services/database/user/updateUser.ts
+++ src/services/database/user/updateUser.ts
@@ -1,8 +1,7 @@
-import { DatabaseUser } from "../model/DatabaseUser";
-import { DTOUser } from "../../../controllers/model/DTOUser";
+import { DatabaseUser, IUser } from "../model/DatabaseUser";
 
-export const updateUserById = async (userId: string, updatedUser: Partial<DTOUser>) => {
-  const result = await DatabaseUser.updateOne({ _id: userId }, { $set: updatedUser });
+export const updateUserById = async (userId: string, updateUser: Partial<IUser>) => {
+  const result = await DatabaseUser.updateOne({ _id: userId }, { $set: updateUser });
 
   console.log(`${result.matchedCount} document(s) matched the query criteria.`);
   console.log(`${result.modifiedCount} document(s) was/were updated.`);

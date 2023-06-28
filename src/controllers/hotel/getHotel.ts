bd6ea0615472784dec5957477ddd238625252672
Tobias Urban <tobias.urban1@gmx.de>
2023-06-28 20:25:28 +0200

    fixed issues of hotel api, lowercase notations

--- src/controllers/hotel/getHotel.ts
+++ src/controllers/hotel/getHotel.ts
@@ -1,15 +1,13 @@
 import { databaseService } from "@services";
 import express from "express";
-import { toDTOHOTEL } from "./toDTOHotel";
+import { toDTOHotel } from "./toDTOHotel";
 
-
-export const getHotel =async (req: express.Request, res: express.Response) => {
+export const getHotel = async (req: express.Request, res: express.Response) => {
   const hotelId = req.params.id;
 
-  const hotel = await databaseService.findHotelById(HotelId);
+  const hotel = await databaseService.findHotelById(hotelId);
   // handle not found in DB
   // res.status(400).send(error);
 
-  res.status(200).json(await toDTOHOTEL(Hotel));
+  res.status(200).json(await toDTOHotel(hotel));
 };
-
